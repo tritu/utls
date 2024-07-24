385a3cba78329b94c4d5c8fe3445cbeded2055d0
tritu <tri_tu@163.com>
2024-07-23 23:10:30 -0700

    Update u_parrots.go
    Signed-off-by: tritu <tri_tu@163.com>

--- u_parrots.go
+++ u_parrots.go
@@ -18,7 +18,7 @@ import (
 	"sort"
 	"strconv"
 
-	"github.com/refraction-networking/utls/dicttls"
+	"github.com/tritu/utls/dicttls"
 )
 
 var ErrUnknownClientHelloID = errors.New("tls: unknown ClientHelloID")
